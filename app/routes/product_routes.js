e91bd44efe8abac04a3fb41065be5c8ef6b8ee8c
adhlira <adhliramadhan68@gmail.com>
2024-02-20 11:41:02 +0700

    add route get put delete product

--- app/routes/product_routes.js
+++ app/routes/product_routes.js
@@ -8,47 +8,71 @@ const router = Router();
 // router.use(express.urlencoded({ extended: true }));
 
 router.get("/product", async (req, res) => {
-  // const results = await prisma.product.findMany();
-  res.json(rootUrl);
+  const results = await prisma.product.findMany();
+  res.json(results);
 });
 
-router.post(
-  "/product",
-  upload.single("image"),
-  validateProductReqBody,
-  async (req, res) => {
-    const { name, description, price, category_id, quantity, rating } =
-      req.body;
-    const rootUrl = `${req.protocol}://${req.get("host")}`;
-    //   console.log(req.body);
-    try {
-      const product = await prisma.$transaction(async (tx) => {
-        const product = await tx.product.create({
+router.post("/product", upload.single("image"), validateProductReqBody, async (req, res) => {
+  const { name, description, price, category_id, quantity, rating } = req.body;
+  const rootUrl = `${req.protocol}://${req.get("host")}`;
+  //   console.log(req.body);
+  try {
+    const product = await prisma.$transaction(async (tx) => {
+      const product = await tx.product.create({
+        data: {
+          name,
+          description,
+          price: +price,
+          category_id: +category_id,
+          quantity: +quantity,
+          rating: +rating,
+        },
+      });
+
+      if (req.file) {
+        await tx.productImage.create({
           data: {
-            name,
-            description,
-            price: +price,
-            category_id: +category_id,
-            quantity: +quantity,
-            rating: +rating,
+            product_id: product.id,
+            image_url: `${rootUrl}/static/${req.file.originalname}`,
           },
         });
+      }
+      return product;
+    });
+    res.json({ product: product, data: "ok" });
+  } catch (error) {
+    fs.unlinkSync(req.file.path);
+  }
+});
 
-        if (req.file) {
-          await tx.productImage.create({
-            data: {
-              product_id: product.id,
-              image_url: `${rootUrl}/static/${req.file.filename}`,
-            },
-          });
-        }
-        return product;
-      });
-      res.json({ product: product, data: "ok" });
-    } catch (error) {
-      fs.unlinkSync(req.file.path);
+router.put("/product/:id", async (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.status(400).json({ message: "Invalid ID" });
+  } else {
+    const product_id = await prisma.product.findFirst({ where: { id: Number(req.params.id) } });
+    if (!product_id) {
+      res.status(404).json({ message: "Product Not Found" });
+    } else {
+      const updated_product = await prisma.product.update({ where: { id: Number(req.params.id) }, data: req.body });
+      res.status(200).json({ message: "Product has been updated", updated_product });
     }
   }
-);
+});
+
+router.delete("/product/:id", async (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.status(400).json({ message: "Invalid ID" });
+  } else {
+    const product_id = await prisma.product.findFirst({ where: { id: Number(req.params.id) } });
+    if (!product_id) {
+      res.status(404).json({ message: "Product Not Found" });
+    } else {
+      const product_id = await prisma.productImage.findFirst({ where: { product_id: Number(req.params.id) } });
+      await prisma.productImage.delete({ where: { id: product_id.id } });
+      await prisma.product.delete({ where: { id: Number(req.params.id) } });
+      res.status(200).json({ message: "Product has been deleted" });
+    }
+  }
+});
 
 export default router;
--- app/validators/validate_register.js
+++ app/validators/validate_register.js
@@ -3,26 +3,31 @@ import validator from "validator";
 const validateRegister = (req, res, next) => {
   const { username, email, password, phone } = req.body;
   if (!username || !email || !password || !phone) {
-    return res.status(400).json({ message: "All fields are required" });
+    return res.status(400).json({
+      message: "All fields are required",
+    });
   }
   if (!validator.isEmail(email)) {
-    return res.status(400).json({ message: "Invalid email" });
+    return res.status(400).json({
+      message: "Invalid email",
+    });
   }
   if (
     !validator.isStrongPassword(password, {
-      minLength: 8,
+      minLength: 6,
       minUppercase: 1,
       minNumbers: 1,
-      minSymbols: 1,
+      minSymbols: 0,
     })
   ) {
     return res.status(400).json({
-      message:
-        "Password must be at least 8 characters long and at least contain 1 uppercase letter, 1 number and 1 symbol",
+      message: "Password must be at least 6 characters long and at least contain 1 uppercase letter, 1 number",
     });
   }
   if (!validator.isMobilePhone(phone)) {
-    return res.status(400).json({ message: "Invalid phone number" });
+    return res.status(400).json({
+      message: "Invalid phone number",
+    });
   }
   next();
 };
